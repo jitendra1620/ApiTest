581175c9828ffe5a9080fe2cc65ad010c62b4382
Jitendra Singh <jitendrasingh@Jitendras-Mac-mini.local>
2019-07-02 22:14:05 +0530

    Filter from josn

--- flask-restfullTest.py
+++ flask-restfullTest.py
@@ -1,26 +1,39 @@
-from flask import Flask, request
+from flask import Flask, jsonify, request, abort
 from flask_restful import Resource, Api
+from taskJson import tasks
 
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
-	def get(self):
-		return {'about':'HelloWorld'}
 
-	def post(self):
-		some_json = request.get_json()
-		return {'you sent' : some_json}, 201
+class GetAllTask(Resource):
+    def get(self):
+        return jsonify({'allTask': tasks})
+
+    def post(self):
+        some_json = request.get_json()
+        return {'you sent': some_json}
+
 
 class Multi(Resource):
-	def get(self, num):
-		return {'result':num*10}
+    def get(self, num):
+        return {'result': num * 10}
 
-api.add_resource(HelloWorld,'/')
-api.add_resource(Multi,'/multi/<int:num>')
+
+class GetTask(Resource):
+    def get(self, taskID):
+	    taskToSend = None
+	    for task in tasks:
+	        if task['id'] == taskID:
+	            taskToSend = task
+	            return jsonify({'task': task})
+	    if taskToSend == None:
+	    	return jsonify({'task': ""})
+
+
+api.add_resource(GetAllTask, '/allTask/')
+api.add_resource(Multi, '/multi/<int:num>')
+api.add_resource(GetTask, '/taskID/<int:taskID>')
 
 if __name__ == '__name__':
-	app.run(debug=True)
-		
-		
-		
+    app.run(debug=True)
